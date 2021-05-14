243ddf564e18afb66ca5034748e715d81b3d1f88
Dorota Tomala <doforever@gmail.com>
2021-05-14 15:45:12 +0200

    Prevent multiple votes

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -62,12 +63,25 @@ exports.vote = async (req, res) => {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
+      const hasVoted = await Voter.exists({ user: req.clientIp });
+      if (!hasVoted) {
+        const voter = new Voter({ user: req.clientIp, votes: [ photoToUpdate._id ] });
+        await voter.save();
+      } else {
+        const voter = await Voter.findOne({ user: req.clientIp });
+        if (voter.votes.includes(photoToUpdate._id)) {
+          throw new Error('Vote duplicate!');
+        } else {
+          voter.votes.push(photoToUpdate._id);
+          await voter.save();
+        }
+      }
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
     }
   } catch(err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message);
   }
 
 };
