c94d3df9795775ca3fbe09f7c3ba586a5a46e4e8
Dorota Tomala <doforever@gmail.com>
2021-05-14 13:27:53 +0200

    Validate title and author length

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -11,12 +11,16 @@ exports.add = async (req, res) => {
     if(title && author && email && file.size !== 0) { // if fields are not empty...
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
-      if (['gif', 'png', 'jpg'].includes(fileExt)) {
+      
+      if ( ['gif', 'png', 'jpg'].includes(fileExt)
+        && title.length <= 25
+        && author.length <= 50
+      ) {
         const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
         await newPhoto.save(); // ...save new photo in DB
         res.json(newPhoto);
       } else {
-        throw new Error('Wrong file type!');
+        throw new Error('Wrong input!');
       }
     } else {
       throw new Error('Wrong input!');
